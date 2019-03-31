f1edd031d7f2ddb5d3dec5f96395b562e63d22fe
Erik Ostermueller <eostermueller@gmail.com>
2019-03-31 15:13:39 -0500

    stable UseCase/pu selection

--- frontend/src/main/web/src/app/use-case-card/use-case-card.component.ts
+++ frontend/src/main/web/src/app/use-case-card/use-case-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { MatCheckboxChange, MatRadioButton, MatRadioChange } from '@angular/material';
 
 
 @Component({
@@ -11,17 +12,72 @@ export class UseCaseCardComponent implements OnInit {
   IsIndeterminate:boolean;
   LabelAlign:string;
   IsDisabled:boolean;
-  selectedStatus: string;
-  @Input() useCase: any;
 
+  public selectedIndex:number = 0;
+  public previousIndex:number = -1;
+  @Input() useCase: any;
+  useCaseObj: any;
   constructor() {
     this.IsChecked = false;
     this.IsIndeterminate = false;
     this.LabelAlign = 'after';
     this.IsDisabled = false;
   }
+  typeOf(obj:any) {
+    return {}.toString.call(obj).split(' ')[1].slice(0, -1).toLowerCase();
+  }
 
   ngOnInit() {
   }
 
+  getCurrentProcessingUnit() {
+    return this.useCase.processingUnits[this.selectedIndex];
+  }
+
+  currentState() {
+    console.log('useCase pr0ps: ' + Object.getOwnPropertyNames(this.useCase));
+    console.log('useCase type a: ' + this.typeOf(this.useCase) );
+    console.log('useCase type b: ' + this.typeOf(this.useCase.name) );
+    console.log('useCase type c: ' + this.typeOf(this.useCase.processingUnits) );
+
+    console.log('Enabled ' + this.IsChecked);
+    console.log( 'UseCase: ' + this.useCase.name );
+    console.log( 'UseCase dump: ' + JSON.stringify(this.useCase) );
+    console.log( 'UseCase pi length: ' + this.useCase.processingUnits.length );
+    const processingUnit = 
+
+    console.log( 'Selected processing impl method name: ' 
+    + processingUnit.method.name);
+
+    console.log( 'Selected processing class name: ' 
+    + processingUnit.method.declaringClassName );
+   }
+  onEnableCheckboxChange(cbEvent: MatCheckboxChange) {
+    if (!this.IsChecked) {
+      if (this.previousIndex!=-1) 
+    }
+//    this.currentState();
+
+      console.log('cb checked ' + cbEvent.checked);
+      console.log('cb name ' + cbEvent.source.name);
+      console.log('cb value ' + cbEvent.source.value);
+      console.log('cb id ' + cbEvent.source.id);
+      console.log('cb inputId ' + cbEvent.source.inputId);
+  }
+  onRadioChange(mrChange: MatRadioChange) {
+    //this.currentState();
+
+    if (this.IsChecked) {
+
+      this.previousIndex = this.selectedIndex;
+    }
+    console.log(mrChange.value);
+    let mrButton: MatRadioButton = mrChange.source;
+    console.log('r name ' + mrButton.name);
+    console.log('r checked ' + mrButton.checked);
+    console.log('r id ' + mrButton.id);
+    console.log('r inputId ' + mrButton.inputId);
+
+ } 
+ 
 }
