8aea3ae02829bade7323b89d521c85c44cb03fd1
Erik Ostermueller <eostermueller@gmail.com>
2019-05-12 09:59:36 -0500

    can flag processingUnit as selected

--- workload/src/main/java/com/github/eostermueller/havoc/workload/annotations/ProcessingUnit.java
+++ workload/src/main/java/com/github/eostermueller/havoc/workload/annotations/ProcessingUnit.java
@@ -11,8 +11,16 @@ import java.lang.annotation.RetentionPolicy;
 @Target({ElementType.METHOD})
 public @interface ProcessingUnit {
 	public static final String USE_CASE = "useCase";
+	public static final String SELECTED = "selected";
 	public static final String VALUE = "value";
 	
  	public String useCase();
+
+ 	public boolean selected() default false;
+ 	/**
+ 	 * Human read-able description of the processing.
+ 	 * one value in this array for each language translation/locale you choose to support.
+ 	 * @return
+ 	 */
  	public UserInterfaceDescription[] value();
 }
