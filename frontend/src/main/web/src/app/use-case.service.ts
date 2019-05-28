7f7021355b76b601c9e075a1e91547cd8694c982
Erik Ostermueller <eostermueller@gmail.com>
2019-05-28 12:03:29 +0200

    correctly tracking workload selection in UseCasesComponent#useCaseSelection

--- frontend/src/main/web/src/app/use-case.service.ts
+++ frontend/src/main/web/src/app/use-case.service.ts
@@ -12,11 +12,24 @@ export class UseCaseService {
 // dataList = new Subject<any[]>();
 
   constructor(private http: HttpClient) {}
+  /**
+   * Alternative approach.  See how the Race class
+   * provides a cast during the json deserialization?
+   * https://blog.ninja-squad.com/2016/03/15/ninja-tips-2-type-your-json-with-typescript/
+   * 
+   * 
+   * 
+    getRaceById(id): Observable<Race> {
+        return this._http.get(`/api/races/${id}`)
+                         .map(response => response.json());
+    }
+   * 
+   */
 
       getUseCases() :Observable<any> {
         //havocAgent/useCases
-        return this.http.get('/havocAgent/useCases');
-        //return this.http.get('http://localhost:8080/workload/useCases');
+        //return this.http.get('/havocAgent/useCases');
+        return this.http.get('http://localhost:8080/workload/useCases');
           // }));
       }
 
