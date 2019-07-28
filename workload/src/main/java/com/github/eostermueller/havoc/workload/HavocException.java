6a657ccdc23c35e4c00b9714de865a1419623856
Erik Ostermueller <eostermueller@gmail.com>
2019-07-28 11:33:21 -0500

    fix for narrow mat text field

--- workload/src/main/java/com/github/eostermueller/havoc/workload/HavocException.java
+++ workload/src/main/java/com/github/eostermueller/havoc/workload/HavocException.java
@@ -1,11 +1,23 @@
 package com.github.eostermueller.havoc.workload;
 
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 public class HavocException extends Exception {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	private String message;
+	Exception causedBy = null;
+	public Exception getCausedBy() {
+		return causedBy;
+	}
+	public void setCausedBy(Exception val) {
+		val.printStackTrace();
+		this.causedBy = causedBy;
+	}
 	public HavocException() {
 		
 	}
@@ -15,6 +27,12 @@ public class HavocException extends Exception {
 	public HavocException(Exception cause, String message) {
 		super(cause);
 		this.message = message;
+	}
+	public HavocException(JsonProcessingException e) {
+		this.setCausedBy(e);
+	}
+	public HavocException(IOException e) {
+		this.setCausedBy(e);
 	}
 	@Override
 	public String getMessage() {
