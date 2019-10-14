3167468ef045ed081cc5cbd0fda81073a732bfc0
Erik Ostermueller <eostermueller@gmail.com>
2019-10-14 18:06:42 -0500

    all 7 zip files

--- packagingMagic.sh
+++ packagingMagic.sh
@@ -5,7 +5,7 @@ LANDING=/Users/erikostermueller/Documents/src/jssource/havoc2/installer/installF
 PM_HOME=/Users/erikostermueller/Documents/src/jssource/havoc2/havoc2/processManager
 JM_HOME=/Users/erikostermueller/Documents/src/jssource/havoc2/havoc2/jmeterFiles
 WM_HOME=/Users/erikostermueller/Documents/src/jssource/havoc2/havoc2/wiremock
-
+MVN_BIN=http://www-us.apache.org/dist/maven/maven-3/3.6.2/binaries/apache-maven-3.6.2-bin.zip
 MVN_REPO=/Users/erikostermueller/.m2
 
 # local location of this repo: https://github.com/eostermueller/tjp2
@@ -15,28 +15,30 @@ TJP_HOME=/Users/erikostermueller/Documents/src/jsource/tjp2
 TARGET=/Users/erikostermueller/Documents/src/jssource/havoc2/havoc2/backend/src/main/resources
 
 echo Creating processManager.zip
-jar cvfM $LANDING/processManager.zip -C $PM_HOME .
+jar cvfM $TARGET/processManager.zip -C $PM_HOME .
 echo processManager.zip is created
 
 echo Creating jmeterFiles.zip
-jar cvfM $LANDING/jmeterFiles.zip -C $JM_HOME .
+jar cvfM $TARGET/jmeterFiles.zip -C $JM_HOME .
 echo jmeterFiles.zip is created
 
 echo Creating wiremockFiles.zip
-jar cvfM $LANDING/wiremockFiles.zip -C $WM_HOME .
+jar cvfM $TARGET/wiremockFiles.zip -C $WM_HOME .
 echo wiremockFiles.zip is created
 
 echo Creating sutApp.zip
-jar cvfM $LANDING/sutApp.zip -C $TJP_HOME .
+jar cvfM $TARGET/sutApp.zip -C $TJP_HOME .
 echo sutApp.zip created.
 
 #
 #  Geez this takes so long, so keep it commented out until absolutely necessary
 #
 echo Creating repository.zip
-jar cvfM $LANDING/repository.zip -C $MVN_REPO repository
+jar cvfM $TARGET/repository.zip -C $MVN_REPO repository
 echo repository.zip is created
 
-cp $LANDING/* $TARGET
+cp $LANDING/data.zip $TARGET/
 
-ls -lart $TARGET
+
+wget --output-document=$TARGET/apache-maven-3.6.2-bin.zip $MVN_BIN
+
