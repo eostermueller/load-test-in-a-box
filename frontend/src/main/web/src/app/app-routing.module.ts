1e43a0acbc67d37145351ad8601f734affb691e0
eostermueller <eostermueller@gmail.com>
2021-01-03 16:22:02 -0600

    updates from ng upgrade to 10

--- frontend/src/main/web/src/app/app-routing.module.ts
+++ frontend/src/main/web/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import {RouterModule} from '@angular/router';
 import {routes} from './app.routes';
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
+  imports: [ RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }) ],
   exports: [ RouterModule ]
 })
 export class AppRoutingModule {
